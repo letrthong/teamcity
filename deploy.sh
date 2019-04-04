1e1acd99e80bc4b394a9758f589ef41193bf8832
etr1hc <Thong.LeTrung@vn.bosch.com>
2019-04-04 18:18:29 +0700

    note

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,25 @@
 # google cloud at https://www.jetbrains.com/teamcity/download/#section=google-cloud
 
 
+#
+#   cd  TeamCity
+#   cd bin 
+#  ./startup.sh
+#    netstat -tulpn
+
 wget https://download-cf.jetbrains.com/teamcity/TeamCity-2018.2.3.tar.gz
 tar xfz TeamCity-2018.2.3.tar.gz
+
+#sudo apt update
+#apt install default-jre
+#sudo apt install default-jdk
+#  java -version
+
+
+# Email SendGrid
+# https://console.cloud.google.com/marketplace/details/sendgrid-app/sendgrid-email?project=theinternetofthing&folder&organizationId
+# port 587 
+# https://sendgrid.com/docs/API_Reference/SMTP_API/getting_started_smtp.html
+
+#User: ThongLT
+#https://app.sendgrid.com/?utm_source=docs
