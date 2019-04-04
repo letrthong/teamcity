d44bdcbb36b0c16d71c26256736d679465d22a23
etr1hc <Thong.LeTrung@vn.bosch.com>
2019-04-04 14:19:16 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#
+# File:
+# 
+# Home page: https://www.jetbrains.com/teamcity/
+# google cloud at https://www.jetbrains.com/teamcity/download/#section=google-cloud
+
+
+wget https://download-cf.jetbrains.com/teamcity/TeamCity-2018.2.3.tar.gz
+tar xfz TeamCity-2018.2.3.tar.gz
